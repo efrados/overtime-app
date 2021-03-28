253faa5d7be71af672e94d597fec160255f6c8f8
Efrain <efrados@gmail.com>
2021-03-28 19:04:57 -0300

    created method full_name for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   validates_presence_of :first_name, :last_name
+
+  def full_name
+    last_name.upcase  + ", " +  first_name.upcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "John",last_name: "Snow")
+  end
+
   describe "creation" do
-    before do
-      @user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "John",last_name: "Snow")
-    end
 
     it "can be created" do
       expect(@user).to be_valid
@@ -14,6 +15,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom method" do
+    it "has a full name method that combines first_name and last_name" do
+      expect(@user.full_name).to eq("SNOW, JOHN")
     end
   end
 end
