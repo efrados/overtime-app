0241c8e1efbc4d354fe427291245947c3af37f36
Efrain casa (S) <efrados@gmail.com>
2021-03-27 21:34:07 -0300

    created new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,28 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to @post
+  end
+
+  def show
+  end
+
+private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:date, :rationale)
   end
 end
