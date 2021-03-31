3758e82cc8f57077abdd8e912689fbee27da76b8
Efrain <efrados@gmail.com>
2021-03-30 23:29:44 -0300

    removed the ability for users to register

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users do
+  devise_for :users, skip: [:registrations] do
     get "/sign_up" => "devise/registrations#new"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
