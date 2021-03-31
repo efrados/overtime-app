127c15f0f18b9bd053c729aa9cd4627cfdda9b1e
Efrain <efrados@gmail.com>
2021-03-30 23:08:33 -0300

    Added Toastr for flash notifications

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+global.toastr = require("toastr")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -10,8 +12,10 @@ require("channels")
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 
+
 document.addEventListener("turbolinks:load", function() {
     $(function () {
+      toastr.error('I do not think that word means what you think it means.', 'Inconceivable!')
         $('[data-toggle="tooltip"]').tooltip();
         $('[data-toggle="popover"]').popover();
         $('[data-toggle="collapse"]').collapse();
