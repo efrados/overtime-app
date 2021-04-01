c1822b1b3bdc8cb8464059d152a4e273940b805e
Efrain <efrados@gmail.com>
2021-04-01 20:04:25 -0300

    Implemented badges with status values for posts_index

---
+++ app/helpers/post_helper.rb
@@ -0,0 +1,16 @@
+module PostHelper
+  def status_label(status)
+    status_span_generator status
+  end
+
+  def status_span_generator (status)
+    case status
+    when 'submitted'
+      content_tag(:span, status.titleize, class: 'badge badge-primary')
+    when 'approved'
+      content_tag(:span, status.titleize, class: 'badge badge-success')
+    when 'rejected'
+      content_tag(:span, status.titleize, class: 'badge badge-danger')
+    end
+  end
+end
